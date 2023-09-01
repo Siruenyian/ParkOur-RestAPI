70bb8525e9809cf85abcb75f082f7c7f4662b7a1
Siruenyian <92420947+Siruenyian@users.noreply.github.com>
2023-09-01 16:04:49 +0700

    feat: update controller and api

--- app/Http/Controllers/ParkirController.php
+++ app/Http/Controllers/ParkirController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Application\Command\BayarParkir\BayarParkirCommand;
+use App\Application\Command\BayarParkir\BayarParkirRequest;
+use App\Application\Command\CariParkir\CariParkirCommand;
+use App\Application\Command\CariParkir\CariParkirRequest;
 use App\Application\Query\ListParkirQuery\ListParkirQueryInterface;
-
+use Exception;
+use Illuminate\Http\Request;
 class ParkirController extends Controller
 {
 
@@ -12,7 +17,8 @@ class ParkirController extends Controller
     )
     {
     }
-    public function search($nama)
+    // function untuk search gedung, harus exact match ig
+    public function CariTempat($nama)
     {
 
         $tempat = $this->listparkirQuery->execute($nama);
@@ -23,4 +29,40 @@ class ParkirController extends Controller
         }
         return response()->json($tempat, 200);
     }
+
+//    test with 1530eb83-5619-451a-b2f7-ffec38183a69, ALKSNDAKJ
+    public function CariParkir(Request $request, CariParkirCommand $command)
+    {
+        $tempatId=$request->input('tempatId');;
+        $platNomor=$request->input('platNomor');;
+        $input = array("1D", "2D", "3D", "4D", "5D");
+        //update transaksi table juga
+        $tempat_parkir=$input[array_rand($input)];
+        $cariparkirRequest = new CariParkirRequest(
+            $tempatId, $platNomor
+        );
+        try {
+            //add retrieve id
+            $id=$command->execute($cariparkirRequest);
+        } catch (Throwable $e) {
+            return back()->withErrors($e->getMessage())->withInput();
+        }
+        return response()->json([$id, $tempat_parkir], 200);
+    }
+//This is for bayar parkir, jgn salah
+    public function BayarParkir(Request $request, BayarParkirCommand $command)
+    {
+        $transaksiId=$request->input('transaksiId');;
+        $platNomor=$request->input('platNomor');;
+        $bayarparkirRequest = new BayarParkirRequest(
+            $transaksiId,$platNomor
+        );
+        try {
+            $command->execute($bayarparkirRequest);
+        } catch (Throwable $e) {
+            return back()->withErrors($e->getMessage())->withInput();
+        }
+        return response()->json("pembayaran sukses", 200);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+//harus/api dulu
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/search/{nama}', [ParkirController::class, 'search']);
+Route::get('/caritempat/{nama}', [ParkirController::class, 'CariTempat']);
+Route::post('/cariparkir', [ParkirController::class, 'CariParkir']);
+Route::post('/bayarparkir', [ParkirController::class, 'BayarParkir']);
